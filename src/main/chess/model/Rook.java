22e3841f0f46308cbb41f48478b0875783da5d6a
Alex Maxwell <maxaman33@gmail.com>
2013-02-26 21:18:01 -0600

    MErging

--- src/main/chess/model/Rook.java
+++ src/main/chess/model/Rook.java
@@ -1,6 +1,7 @@
 package main.chess.model;
 
 import java.awt.Point;
+import java.util.HashSet;
 import java.util.Set;
 
 import main.chess.common.Constants;
@@ -15,8 +16,30 @@ public class Rook extends ChessPiece {
 
 	@Override
 	public Set<Point> getMovePositions(Board b) {
-		// TODO Auto-generated method stub
-		return null;
+		// TODO Auto-generated method stub		
+		Set<Point> locs = new HashSet<Point>();
+		Point myLoc = this.getLocation();
+		Point toAdd = new Point(myLoc);
+		//check spaces to the right
+		for(int i = 1; toAdd.getX() + i <= 8; i++){
+	        toAdd.setLocation(myLoc.getX() + i,myLoc.getY());
+	        if(b.isOnBoard(toAdd) && b.getBlock(toAdd).getPiece() == null)
+	        	locs.add(toAdd);
+		}
+		//check spaces to the left
+		for(int i = 1; toAdd.getX() + i >= 0; i++){
+	        toAdd.setLocation(myLoc.getX() - i,myLoc.getY());
+	        if(b.isOnBoard(toAdd) && b.getBlock(toAdd).getPiece() == null)
+	        	locs.add(toAdd);
+		}
+		//check spaces forward (remember Enum)
+		for(int i = 0; toAdd.getY() + i < 8; i++){
+		    //TODO	
+		}
+		//check spaces backward
+		
+		
+		return locs;
 	}
 
 	@Override
