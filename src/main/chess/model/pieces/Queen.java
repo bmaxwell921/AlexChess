daa2e9423464b4933dec161ba80a0d599e4e7102
Maxaman33 <maxaman33@gmail.com>
2013-10-04 22:16:21 -0700

    Queen is done.  Literally copy-pasted Bishop and Rook...So it's pretty ugly...will try to fix later.

--- src/main/chess/model/pieces/Queen.java
+++ src/main/chess/model/pieces/Queen.java
@@ -18,7 +18,66 @@ public class Queen extends ChessPiece {
 		LocationCollection locs = new LocationCollection();
 		Point myLoc = this.getLocation();
 		Point toAdd = new Point(myLoc);
-		
+		//check spaces to the right
+		for(int i = 1; myLoc.getX() + i < 8; i++){
+		      toAdd.setLocation(myLoc.getX() + i,myLoc.getY());
+			  if(board.isOnBoard(toAdd) && board.getBlock(toAdd).getPiece() == null)
+			      	locs.add(new Point(toAdd));
+			  if(board.isOnBoard(toAdd) && board.getBlock(toAdd).getPiece() != null)
+			        break;//if there is a piece there, rooks can't jump pieces.
+		}
+		//check spaces to the left
+		for(int i = 1; myLoc.getX() - i >= 0; i++){
+			toAdd.setLocation(myLoc.getX() - i,myLoc.getY());
+			  if(board.isOnBoard(toAdd) && board.getBlock(toAdd).getPiece() == null)
+			  	locs.add(new Point(toAdd));
+			       if(board.isOnBoard(toAdd) && board.getBlock(toAdd).getPiece() != null)
+			    	   break;//if there is a piece there, rooks can't jump pieces.
+		}
+		//check spaces forward (remember Enum) ??? 
+		for(int i = 1; myLoc.getY() + i < 8; i++){
+		    toAdd.setLocation(myLoc.getX(), myLoc.getY() + i);
+		    if(board.isOnBoard(toAdd) && board.getBlock(toAdd).getPiece() == null)
+		    	locs.add(new Point(toAdd));
+		    if(board.isOnBoard(toAdd) && board.getBlock(toAdd).getPiece() != null)
+			    break;//if there is a piece there, rooks can't jump pieces.
+		}
+		//check spaces backward
+		for(int i = 1; myLoc.getY() - i >= 0; i++){
+			toAdd.setLocation(myLoc.getX(), myLoc.getY() - i);
+			if(board.isOnBoard(toAdd) && board.getBlock(toAdd).getPiece() == null)
+				locs.add(new Point(toAdd));
+			if(board.isOnBoard(toAdd) && board.getBlock(toAdd).getPiece() != null)
+			  	break;//if there is a piece there, rooks can't jump pieces.
+		}
+		for(int i = 1; myLoc.getX() - i >= 0 && myLoc.getY() - i >= 0; i++){
+			toAdd.setLocation(myLoc.getX() - i, myLoc.getY() - i);
+			if(board.isOnBoard(toAdd) && board.getBlock(toAdd).getPiece() == null)
+				locs.add(new Point(toAdd));
+			if(board.isOnBoard(toAdd) && board.getBlock(toAdd).getPiece() != null)
+			    break;
+		}
+		for(int i = 1; myLoc.getX() + i < 8 && myLoc.getY() + i < 8; i++){
+			toAdd.setLocation(myLoc.getX() + i, myLoc.getY() + i);
+			if(board.isOnBoard(toAdd) && board.getBlock(toAdd).getPiece() == null)
+				locs.add(new Point(toAdd));
+			if(board.isOnBoard(toAdd) && board.getBlock(toAdd).getPiece() != null)
+		     	break;
+		}
+		for(int i = 1; myLoc.getX() - i >= 0 && myLoc.getY() + i < 8; i++){
+			toAdd.setLocation(myLoc.getX() - i, myLoc.getY() + i);
+			if(!toAdd.equals(myLoc) && board.isOnBoard(toAdd) && board.getBlock(toAdd).getPiece() == null)
+				locs.add(new Point(toAdd));
+			if(board.isOnBoard(toAdd) && board.getBlock(toAdd).getPiece() != null)
+		     	break;
+		}
+		for(int i = 1; myLoc.getX() + i < 8 && myLoc.getY() - i >= 0; i++){
+			toAdd.setLocation(myLoc.getX() + i, myLoc.getY() - i);
+			if(!toAdd.equals(myLoc) && board.isOnBoard(toAdd) && board.getBlock(toAdd).getPiece() == null)
+				locs.add(new Point(toAdd));
+			if(board.isOnBoard(toAdd) && board.getBlock(toAdd).getPiece() != null)
+		    	break;
+		}
 		return locs;
 	}
 
@@ -27,7 +86,106 @@ public class Queen extends ChessPiece {
 		LocationCollection locs = new LocationCollection();
 		Point myLoc = this.getLocation();
 		Point toAdd = new Point(myLoc);
-		
+		//check right for opponents
+		for(int i = 1; myLoc.getX() + i < 8; i++){
+			toAdd.setLocation(myLoc.getX() + i, myLoc.getY());
+			//add if the spot is real and it has an opponent
+			if(board.isOnBoard(toAdd) && board.getBlock(toAdd).getPiece() != null 
+					&& this.isOpponent(board.getBlock(toAdd).getPiece())){
+				locs.add(new Point(toAdd));
+				break;//can only attack one opponent!
+			}
+			//stop searching if the spot is real and it has a non-opponent
+			if(board.isOnBoard(toAdd) && board.getBlock(toAdd).getPiece() != null
+					&& !this.isOpponent(board.getBlock(toAdd).getPiece()))
+				break;
+		}
+		//check left
+		for(int i = 1; myLoc.getX() - i >= 0; i++){
+			toAdd.setLocation(myLoc.getX() - i, myLoc.getY());
+			//add if the spot is real and it has an opponent
+			if(board.isOnBoard(toAdd) && board.getBlock(toAdd).getPiece() != null 
+					&& this.isOpponent(board.getBlock(toAdd).getPiece())){
+				locs.add(new Point(toAdd));
+				break;//can only attack one opponent!
+			}
+			//stop searching if the spot is real and it has a non-opponent
+			if(board.isOnBoard(toAdd) && board.getBlock(toAdd).getPiece() != null
+					&& !this.isOpponent(board.getBlock(toAdd).getPiece()))
+				break;
+		}
+		//check forward
+		for(int i = 1; myLoc.getY() + i < 8; i++){
+			toAdd.setLocation(myLoc.getX(), myLoc.getY() + i);
+			//add if the spot is real and it has an opponent
+			if(board.isOnBoard(toAdd) && board.getBlock(toAdd).getPiece() != null 
+					&& this.isOpponent(board.getBlock(toAdd).getPiece())){
+				locs.add(new Point(toAdd));
+			break;//can only attack one opponent!
+		}
+			//stop searching if the spot is real and it has a non-opponent
+			if(board.isOnBoard(toAdd) && board.getBlock(toAdd).getPiece() != null
+					&& !this.isOpponent(board.getBlock(toAdd).getPiece()))
+				break;
+		}
+		//check backward
+		for(int i = 1; myLoc.getY() - i >= 0; i++){
+			toAdd.setLocation(myLoc.getX(), myLoc.getY() - i);
+			//add if the spot is real and it has an opponent
+			if(board.isOnBoard(toAdd) && board.getBlock(toAdd).getPiece() != null 
+					&& this.isOpponent(board.getBlock(toAdd).getPiece())){
+				locs.add(new Point(toAdd));
+				break;//can only attack one opponent!
+			}
+			//stop searching if the spot is real and it has a non-opponent
+			if(board.isOnBoard(toAdd) && board.getBlock(toAdd).getPiece() != null
+					&& !this.isOpponent(board.getBlock(toAdd).getPiece()))
+				break;
+		}
+		for(int i = 1; i < 8; i++){
+			toAdd.setLocation(myLoc.getX() + i, myLoc.getY() + i);
+			if(board.isOnBoard(toAdd)){
+				if(isOpponent(board.getBlock(toAdd).getPiece())){
+					locs.add(new Point(toAdd));
+					break;
+				}
+				if(board.getBlock(toAdd).getPiece() != null)
+					break;
+			}
+		}
+		for(int i = 1; i < 8; i++){
+			toAdd.setLocation(myLoc.getX() - i, myLoc.getY() - i);
+			if(board.isOnBoard(toAdd)){
+				if(isOpponent(board.getBlock(toAdd).getPiece())){
+					locs.add(new Point(toAdd));
+					break;
+				}
+				if(board.getBlock(toAdd).getPiece() != null)
+					break;
+			}
+		}
+		for(int i = 1; i < 8; i++){
+			toAdd.setLocation(myLoc.getX() - i, myLoc.getY() + i);
+			if(board.isOnBoard(toAdd)){
+				if(isOpponent(board.getBlock(toAdd).getPiece())){
+					locs.add(new Point(toAdd));
+					break;
+				}
+				if(board.getBlock(toAdd).getPiece() != null)
+					break;
+			}
+		}
+		for(int i = 1; i < 8; i++){
+			toAdd.setLocation(myLoc.getX() + i, myLoc.getY() - i);
+			if(board.isOnBoard(toAdd)){
+				if(isOpponent(board.getBlock(toAdd).getPiece())){
+					locs.add(new Point(toAdd));
+					break;
+				}
+				if(board.getBlock(toAdd).getPiece() != null)
+					break;
+			}
+		}
 		return locs;
 	}
 
